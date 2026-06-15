2463f527eb2638468944661b96f19944c2ba1981
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2026-06-15 22:28:05 +0200

    Change Termux app package name to dev.mutwakil.androidide

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -30,7 +30,7 @@ else
 fi
 
 # Termux packages configuration.
-TERMUX_APP_PACKAGE="com.itsaky.androidide"
+TERMUX_APP_PACKAGE="dev.mutwakil.androidide"
 TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
 TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
